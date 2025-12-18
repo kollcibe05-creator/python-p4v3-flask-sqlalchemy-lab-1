1b45d536a38977778633db4055999bcb3e31ab8f
Collins Kibet <kollcibe05@gmail.com>
2025-12-18 08:53:57 +0300

    created the model class Earthquake

--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, Column, Integer, String, Float
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
@@ -7,3 +7,17 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes"
+    id = Column(Integer(), primary_key=True)
+    magnitude = Column(Float())
+    location = Column(String())
+    year = Column(Integer())
+
+    # Serialization rules which is optional: prevents recursion if you add relationships
+    serialize_rules = ()
+
+    def __repr__(self):
+        return f"""<
+                {self.id}, {self.magnitude}, {self.location}, {self.year}>                    
+                """
