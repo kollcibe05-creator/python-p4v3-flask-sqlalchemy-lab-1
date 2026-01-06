84e9985e0c701f02bc153664c54ca3237e51e755
Collins Kibet <kollcibe05@gmail.com>
2026-01-06 14:32:31 +0300

    solved test

--- server/app.py
+++ server/app.py
@@ -21,6 +21,40 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
+
+    if earthquake:
+        response_body = {
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }
+        status_code = 200
+
+    else:
+        response_body = ({"message": f"Earthquake {id} not found."})
+        status_code = 404
+    return make_response(response_body, status_code)    
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def earthquake_by_magnitude(magnitude):
+    list_ = []
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    for quake in quakes:
+        body = {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            'year': quake.year
+        }
+        list_.append(body)
+    response_body = {
+        "count": len(quakes),
+        "quakes": list_
+    }    
+    return make_response(response_body, 200)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData, Column, Integer, String, Float
+from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
@@ -9,15 +9,11 @@ db = SQLAlchemy(metadata=metadata)
 # Add models here
 class Earthquake(db.Model, SerializerMixin):
     __tablename__ = "earthquakes"
-    id = Column(Integer(), primary_key=True)
-    magnitude = Column(Float())
-    location = Column(String())
-    year = Column(Integer())
-
-    # Serialization rules which is optional: prevents recursion if you add relationships
-    serialize_rules = ()
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
 
     def __repr__(self):
-        return f"""<
-                {self.id}, {self.magnitude}, {self.location}, {self.year}>                    
-                """
+        return f"<{self.id}, {self.magnitude}, {self.location}, {self.year}>"
+
